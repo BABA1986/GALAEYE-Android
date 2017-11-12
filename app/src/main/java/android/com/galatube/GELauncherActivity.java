c66321b4553a69a84b4fa09017ee83c3989f9487
BABA1986 <baba.rathore1986@gmail.com>
2017-11-13 01:21:21 +0530

    - splash added.

--- app/src/main/java/android/com/galatube/GELauncherActivity.java
+++ app/src/main/java/android/com/galatube/GELauncherActivity.java
@@ -14,6 +14,9 @@ import android.support.v7.app.ActionBar;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by deepak on 30/11/16.
  */
@@ -27,37 +30,37 @@ public class GELauncherActivity extends Activity
         applyTheme();
         GESharedMenu lSharedMenu = GESharedMenu.getInstance(getApplicationContext());
 
-        Thread t = new Thread(      new Runnable() {
+        new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                //  Initialize SharedPreferences
-                SharedPreferences getPrefs = PreferenceManager
-                        .getDefaultSharedPreferences(getBaseContext());
+                        //  Initialize SharedPreferences
+                        SharedPreferences getPrefs = PreferenceManager
+                                .getDefaultSharedPreferences(getBaseContext());
 
-                //  Create a new boolean and preference and set it to true
-                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
-                isFirstStart = true; //Temporary
-                //  If the activity has never started before...
-                if (isFirstStart)
-                {
-                    //  Launch app intro
-                    Intent i = new Intent(GELauncherActivity.this, GEIntroActivity.class);
-                    startActivity(i);
+                        //  Create a new boolean and preference and set it to true
+                        boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+                        isFirstStart = true; //Temporary
+                        //  If the activity has never started before...
+                        if (isFirstStart)
+                        {
+                            //  Launch app intro
+                            Intent i = new Intent(GELauncherActivity.this, GEIntroActivity.class);
+                            startActivity(i);
+                            finish();
 
-                    //  Make a new preferences editor
-                    SharedPreferences.Editor e = getPrefs.edit();
+                            //  Make a new preferences editor
+                            SharedPreferences.Editor e = getPrefs.edit();
 
-                    //  Edit preference to make it false because we don't want this to run again
-                    e.putBoolean("firstStart", false);
+                            //  Edit preference to make it false because we don't want this to run again
+                            e.putBoolean("firstStart", false);
 
-                    //  Apply changes
-                    e.apply();
-                }
-            }
-        });
+                            //  Apply changes
+                            e.apply();
+                        }
+                    }
+        }, 3000);
 
-        // Start the thread
-        t.start();
+
     }
 
     private void applyTheme() {
