62fd5fe2dc0d55658f46fc2a03e2e3866e17be47
ravi.rathore <ravi.rathore@flipkart.com>
2020-04-26 03:35:32 +0530

    changes

--- app/src/main/java/gala/com/kidstv/Refactored/items/ThumbnailItems/ThumbnailViewHolder.java
+++ app/src/main/java/gala/com/kidstv/Refactored/items/ThumbnailItems/ThumbnailViewHolder.java
@@ -1,11 +1,22 @@
 package gala.com.kidstv.Refactored.items.ThumbnailItems;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.github.vivchar.rendererrecyclerviewadapter.ViewHolder;
 
+import gala.com.kidstv.R;
+
 public class ThumbnailViewHolder extends ViewHolder {
+    private final TextView mTvTitle;
+    private final TextView mTvDuration;
+    private final ImageView mIvThumbnail;
+
     public ThumbnailViewHolder(final View itemView) {
         super(itemView);
+        mTvTitle = itemView.findViewById(R.id.tv_title);
+        mTvDuration = itemView.findViewById(R.id.tv_duration);
+        mIvThumbnail  = itemView.findViewById(R.id.iv_thumbnail);
     }
 }
